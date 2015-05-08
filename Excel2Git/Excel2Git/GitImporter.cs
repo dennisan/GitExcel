e1acfe447ee1515e30d26780954447aea3014cd6
Dennisan <dangeline@live.com>
2015-05-07 17:37:51 -0700

    initial commit

---
+++ Excel2Git/Excel2Git/GitImporter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Excel2Git
+{
+    using Octokit;
+
+    static class GitRepo
+    {
+        public static int ImportXls(string xlsPath, Uri repoUri)
+        {
+            int recordsImported = 0;
+
+            // test xls file
+            // test repo name
+
+            if (File.Exists(xlsPath))
+            {
+                var client = new GitHubClient(new ProductHeaderValue("mspnp-importer"), repoUri);
+
+                var user = client.User.Current();
+
+
+
+            }
+            else
+            {
+                Console.WriteLine("Xls file not found [{0}]", xlsPath);
+            }
+
+            return recordsImported;
+        }
+    }
+}
---
+++ Excel2Git/Excel2Git/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel2Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string xlsFile = "";
+            string repoName = "";
+
+            if (args.Length == 2)
+            {
+                xlsFile = args[0];
+                repoName = args[1];
+            }
+
+            Console.WriteLine("Importing backlog items from {0} to Git Repo {1}", xlsFile, repoName);
+
+            int recordsImported = GitRepo.ImportXls(xlsFile, new Uri(repoName));
+
+            Console.WriteLine("{0} issues imported in the Git repo {1}", recordsImported, repoName);
+        }
+    }
+}
